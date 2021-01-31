8b116d4deef0531f022067771d5932bc913f185b
Elliott Vargo <vargoelliott@gmail.com>
2021-01-31 00:27:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS.each do |i|
-      if i != brand || BRANDS == []
+      if i != brand 
         BRANDS << brand
       end
     end

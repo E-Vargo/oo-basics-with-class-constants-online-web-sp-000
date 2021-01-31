af7d200ded203d4c8f42b3f06ff7056122aa69fa
Elliott Vargo <vargoelliott@gmail.com>
2021-01-31 00:21:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,12 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.each do |i|
-      if i != brand
-        BRANDS << i
-      end
+    BRANDS << brand
+
   end
-end
 
   def cobble
     self.condition = "new"
